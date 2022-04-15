7dd648e11524c2f30f6f633eccf15b51147e9ca0
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-04-15 14:01:42 -0500

    feat: update style

--- src/page/Client/NewClient/index.jsx
+++ src/page/Client/NewClient/index.jsx
@@ -5,7 +5,7 @@ const index = () => {
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900 mb-3">New Client</h1>
-      <p className="mb-10">Fill in the following fields to create a client</p>
+      <p className="mb-7 text-2xl">Fill in the following fields to create a client</p>
       <FormClient />
     </>
   );
