98935b1c8f3d7293788e235684ec98055ca81856
Andres Jaramillo <JaramilloG1999@outlook.com>
2022-04-17 13:34:19 -0500

    feat: create structure component view client and implement logic

---
+++ src/page/Client/ViewClient/index.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getSpecificClient } from "../../../helpers/clients";
+import MoonLoader from "react-spinners/ClipLoader";
+import personalInfo from "../../../assets/images/personal.png";
+
+const index = () => {
+  const { id } = useParams();
+  const [client, setClient] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getSpecificClient(id, setClient, setLoading);
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <div className="flex items-center justify-center">
+          <MoonLoader size={150} color={"#4f46e5"} loading={loading} />
+        </div>
+      ) : (
+        <div className="w-full lg:w-3/4 shadow-sm py-6 px-3 m-auto bg-white rounded">
+          <h1 className="text-2xl text-center uppercase font-bold mt-5">
+            Details client
+          </h1>
+          <div className="flex flex-col xl:flex-row items-center gap-7">
+            <img src={personalInfo} alt="personal-info" />
+            <div className="p-4">
+              <p className="text-xl mb-2 uppercase">
+                <span className="font-bold">Name: </span>
+                {client.name}
+              </p>
+              <p className="text-xl mb-2 uppercase">
+                <span className="font-bold">Business: </span>
+                {client.business}
+              </p>
+              <p className="text-xl mb-2 uppercase">
+                <span className="font-bold">Phone: </span>
+                {client.phone}
+              </p>
+              <p className="text-xl mb-2 uppercase ">
+                <span className="font-bold">Email: </span>
+                {client.email}
+              </p>
+              {client.notes ? (
+                <p className="text-xl mb-2 uppercase">
+                  <span className="font-bold">Notes: </span>
+                  {client.notes}
+                </p>
+              ) : null}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default index;
